1c130e46bfc6cc06fbbe94db0a416b11020bee7c
david murphy <davidmurphy1088@gmail.com>
2024-04-15 16:49:38 +1200

    Initial Commit

---
+++ ScalesTrainer/ScalesTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScalesTrainerApp.swift
+//  ScalesTrainer
+//
+//  Created by David Murphy on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ScalesTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
